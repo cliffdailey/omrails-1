ba0f6b719b0c683f7f479c1f42ce4ebd357c4a0f
Clayton Stone <claytonrstone@gmail.com>
2013-02-15 17:54:19 -0600

    Correct s3.rb to reflect correct production level variables for s3 storage

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,9 @@
 # initializers/s3.rb
 if Rails.env == "production"
   # set credentials from ENV hash
-  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "sharedearth-production"}
+  S3_CREDENTIALS = { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :bucket => ENV['AWS_BUCKET']}
 else
   # get credentials from YML file
   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
 end
+
