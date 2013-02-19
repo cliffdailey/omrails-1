91cc13c6abd2f5fb7739eb40ac7cff5a93f4f7d8
Clayton Stone <claytonrstone@gmail.com>
2013-02-18 21:48:50 -0600

    Add validation check for SSL image URLs on image upload

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -6,17 +6,24 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :storage => :s3, :s3_credentials => S3_CREDENTIALS, styles: { large: '600x400>', medium: '320x240>', thumb: '100x100>'}
 
-	before_validation :download_remote_image, :if => lambda { |pin| pin.image_url.present? }
+	before_validation :remote_url_is_ssl, :if => lambda { |pin| pin.image_url.present? }
+	before_validation :download_remote_image, :if => lambda { |pin| pin.image_url.present? }  
   
   validates :description, presence: true
   validates :user_id, presence: true
   validates_attachment :image, presence: true, content_type: { content_type: [ 'image/jpeg', 'image/jpg', 'image/png', 'image,gif', 'image/JPG']},
-  											size: { less_than: 5.megabytes }
+  											size: { less_than: 5.megabytes }, :if => Proc.new{|pin| pin.errors.empty?}
 
   belongs_to :user
   
   
   private
+  def remote_url_is_ssl
+  	if self.image_url.match(/^https/)
+  		errors.add(:image_url, 'Secure image links are not allowed at this time.')
+  	end
+  end
+
   def download_remote_image
     return if self.image.present?
     begin
