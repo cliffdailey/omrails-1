1511976b7edbc22ac3198efbbe5b7b3691b67750
Clayton Stone <claytonrstone@gmail.com>
2013-02-25 18:05:01 -0600

    Add link to view your likes on one page

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,12 @@
 class LikesController < ApplicationController
+	def index
+		@likes = current_user.likes.map { |like| like.pin_id }
+		@pins = @likes.map { |pin| Pin.find(pin) }
+	end
 	def create 
 		@pin = Pin.find(params[:pin_id])
 		@like = current_user.likes.build(:pin_id => params[:pin_id])
-		if @like.save
+		if !@pin.liking_users.include? current_user && @like.save
 			respond_to do |format|
 	      format.html { redirect_to root_url }
     	end
